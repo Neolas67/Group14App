bb803a339f286ebdb7a77fdd5e3eb3bc9f7675a1
Boris Li <boriskenli@gmail.com>
2017-09-20 14:05:18 +1000

    unused library import deleted

--- app/src/main/java/au/edu/uts/redylog/redylog/Fragments/ViewEntryFragment.java
+++ app/src/main/java/au/edu/uts/redylog/redylog/Fragments/ViewEntryFragment.java
@@ -10,7 +10,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import au.edu.uts.redylog.redylog.DataManagers.JournalManager;
